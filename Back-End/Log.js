03ffca9eca8893e85797429e2a8bdb1155483d46
nguyendangkhoa <nguyendankkhoa@gmail.com>
2026-03-14 13:47:59 +0700

    Creating the activity log table

---
+++ Back-End/Log.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const logSchema = new mongoose.Schema({
+  timeStamp: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  activity: {
+    type: String,
+    default: '',
+  },
+  user: {
+    type: String,
+    required: true,
+  },
+});
